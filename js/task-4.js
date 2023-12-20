b0161c2bd4fcfc4d703b19edeedd12519c25cdb0
Alex <112724167+klimbass@users.noreply.github.com>
2023-12-20 22:57:34 +0200

    add_hw_5

---
+++ js/task-4.js
@@ -0,0 +1,51 @@
+const getTotalBalanceByGender = (users, gender) => {
+  const totalBalanceUsers = users
+    .filter((user) => user.gender === gender)
+    .reduce((balance, user) => {
+      return balance + user.balance;
+    }, 0);
+
+  return totalBalanceUsers;
+};
+
+const allUsers2 = [
+  {
+    name: "Moore Hensley",
+    gender: "male",
+    balance: 2811,
+  },
+  {
+    name: "Sharlene Bush",
+    gender: "female",
+    balance: 3821,
+  },
+  {
+    name: "Ross Vazquez",
+    gender: "male",
+    balance: 3793,
+  },
+  {
+    name: "Elma Head",
+    gender: "female",
+    balance: 2278,
+  },
+  {
+    name: "Carey Barr",
+    gender: "male",
+    balance: 3951,
+  },
+  {
+    name: "Blackburn Dotson",
+    gender: "male",
+    balance: 1498,
+  },
+  {
+    name: "Sheree Anthony",
+    gender: "female",
+    balance: 2764,
+  },
+];
+
+console.log(getTotalBalanceByGender(allUsers2, "male")); // 12053
+
+console.log(getTotalBalanceByGender(allUsers2, "female")); // 8863
